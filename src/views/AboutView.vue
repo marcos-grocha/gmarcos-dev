796bf2af9a727365b15d62e8c6d60906efd67235
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2025-02-25 13:35:31 -0300

    Ajusta página about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="about">
-    <h1>Esta é a página "Sobre"</h1>
-    <p>
-      Especialista em Ruby on Rails, construindo software/aplicativos web através de Desenvolvimento Orientado a Testes (Test Driven Development) e práticas ágeis, garantindo a qualidade do código.
-    </p>
-    <p>
-      Apesar da minha especialização, sou um profissional ávido por conhecimento, determinado, autodidata, adaptável e organizado. Apaixonado pelo processo de aprendizagem em novas tecnologias e motivado a buscar excelência em meus objetivos na área.
-    </p>
-  </div>
-</template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <v-card elevation="4" class="pa-4">
+          <v-card-title class="text-h4">
+            Esta é a página "Sobre"
+          </v-card-title>
+          
+          <v-card-subtitle class="text-h6 mt-2">
+            Ruby on Rails + VueJS
+          </v-card-subtitle>
+          <v-card-text>
+            Especialista em Ruby on Rails, construindo software/aplicativos web através de Desenvolvimento Orientado a Testes (Test Driven Development) e práticas ágeis, garantindo a qualidade do código.
+          </v-card-text>
+          <v-card-text>
+            Apesar da minha especialização, sou um profissional ávido por conhecimento, determinado, autodidata, adaptável e organizado. Apaixonado pelo processo de aprendizagem em novas tecnologias e motivado a buscar excelência em meus objetivos na área.
+          </v-card-text>
 
-<style>
-  p {
-    margin: 40px 0;
-  }
-</style>
+          <v-card-subtitle class="text-h6 mt-2">
+            Java SpringBoot + Angular
+          </v-card-subtitle>
+          <v-card-text>
+            Eu também gosto muito de Java, e o bônus é que dá dinheiro
+          </v-card-text>
+          <v-card-text>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro obcaecati corrupti aliquam sint neque! Illum, ratione accusamus? Reprehenderit optio labore atque repudiandae neque quisquam, pariatur aliquid impedit a quam consectetur..
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
