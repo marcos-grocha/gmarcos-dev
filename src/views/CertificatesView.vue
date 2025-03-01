52b40ff54ce77f763515c9416dddc91d2a60962e
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2025-03-01 01:41:15 -0300

    Ajustando certificados e projetos

--- src/views/CertificatesView.vue
+++ src/views/CertificatesView.vue
@@ -2,44 +2,95 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="12" md="8">
+
+        <p class="text-center text-h4">Graduação e Especializações</p><br>
         <v-card elevation="4" class="pa-4">
-          <v-card-title class="text-h4">
-            Certificados
+          <v-card-title class="text-h6">
+            Superior de Tecnologia em Jogos Digitais
           </v-card-title>
-          
-          <v-card-subtitle class="text-h6 mt-2">
+          <v-card-subtitle class="text-subtitle-1 mt-2">
+            Universidade Tiradentes - Concluído em 2024.2
+          </v-card-subtitle>
+          <v-expansion-panels v-model="panel" flat>
+            <v-expansion-panel>
+              <v-expansion-panel-title>Competências do Curso</v-expansion-panel-title>
+              <v-expansion-panel-text>
+                <v-table density="compact">
+                  <thead>
+                    <tr>
+                      <th class="text-left">Módulo</th>
+                      <th class="text-left">Carga Horária</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="competence in skills" :key="competence.module">
+                      <td>{{ competence.module }}</td>
+                      <td>{{ competence.hours }}</td>
+                    </tr>
+                  </tbody>
+                </v-table>
+              </v-expansion-panel-text>
+            </v-expansion-panel>
+            <br>
+            <p class="text-caption">Código de validação: 398.398.fa8a23e84690cd025191d8e29381f42b7db5db92226ebf9a99f549ee5b80ffb5</p>
+          </v-expansion-panels>
+        </v-card>
+
+        <v-divider :thickness="20" class="border-opacity-0"></v-divider>
+
+        <v-card elevation="4" class="pa-4">
+          <v-card-title class="text-h6">
+            Treinamento Imersivo em Desenvolvimento Web - TreinaDev - Turma 13
+          </v-card-title>
+          <v-card-subtitle class="text-subtitle-1">
+            Campus Code - Concluído em 2025.1
+          </v-card-subtitle>
+          <v-expansion-panels flat>
+            <v-expansion-panel
+              title="Competências do Curso"
+              text="Desenvolvimento Web com foco em Ruby on Rails, cobrindo programação em Ruby com boas práticas, desenvolvimento web utilizando o framework Rails, controle de versão com Git, criação e integração de APIs, Test Driven Development (TDD), pair programming, metodologias ágeis (como Scrum e Kanban) além de experiência prática em um time de desenvolvimento."
+              >
+            </v-expansion-panel>
+            <br>
+            <p class="text-caption">Chave de acesso: disponível em breve</p>
+          </v-expansion-panels>
+        
+          <v-divider :thickness="20" class="border-opacity-0"></v-divider>
+
+          <v-card-title class="text-h6">
             Java COMPLETO Programação Orientada a Objetos + Projetos
-          </v-card-subtitle>
-          <v-card-text>
-            Udemy <br>
-            Competências: Java, OO, UML, JDBC, JavaFX, Spring Boot, JPA, Hibernate, MySQL, MongoDB, Maven, Spring Data JPA e Spring Data MongoDB.
-          </v-card-text>
-          <v-card-text>
-            Verificação emitida em jun de 2024 <br>
-            Código da credencial: UC-5a93c6e1-e4d1-4e04-b261-81e7d739d6c3
-          </v-card-text>
-
-          <v-card-title class="text-h4">
-            Projetos
           </v-card-title>
-          <v-card-subtitle class="text-h6 mt-2">
-            E-Ventos | Treina-Dev 13
+          <v-card-subtitle class="text-subtitle-1">
+            Udemy - Concluído em 2024.1
           </v-card-subtitle>
-          
-          <v-card-text>
-            Associado(s) a Campus Code <br>
-            Projeto integrado composto por três aplicações desenvolvidas em Ruby on Rails: events-app, speakers-app e participants-app.
-          </v-card-text>
-          <v-card-text>
-            Nossa equipe de desenvolvedores ficou responsável pela aplicação events-app (E-Ventos) <br>
-
-            O events-app é um sistema completo de gerenciamento de eventos, permitindo que organizadores criem e gerenciem eventos de forma eficiente. O aplicativo suporta a criação de contas de organizadores, cadastro de eventos, configuração de ingressos e agendas, além da verificação de usuários. Administradores têm acesso a ferramentas para moderar usuários, gerenciar categorias de eventos e configurar palavras-chave para facilitar buscas. O sistema também controla limites de participantes e lotes de ingressos, com diferentes opções e preços.
-          </v-card-text>
-          <v-card-text>
-            jan de 2025 - fev de 2025
-          </v-card-text>
+          <v-expansion-panels flat>
+            <v-expansion-panel
+              title="Competências do Curso"
+              text="Java, OO, UML, JDBC, JavaFX, Spring Boot, JPA, Hibernate, MySQL, MongoDB, Maven, Spring Data JPA e Spring Data MongoDB."
+              >
+            </v-expansion-panel>
+            <br>
+            <p class="text-caption">Código da credencial: UC-5a93c6e1-e4d1-4e04-b261-81e7d739d6c3</p>
+          </v-expansion-panels>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        panel: null,
+        skills: [
+          { module: 'Animação e Modelagem de Jogos', hours: 480, },
+          { module: 'Roteiros, Worldbuilding, Programação, Ilustração e Negócios', hours: 480, },
+          { module: 'UI/UX, Animação e Desenvolvimento de Jogos', hours: 480, },
+          { module: 'Fundamentos de IA e de Game Design', hours: 480, },
+          { module: 'Fundamentos iniciais no Design de Jogos', hours: 480, },
+        ],
+      }
+    },
+  }
+</script>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -89,6 +89,30 @@
               </v-list-item>
             </v-list>
 
+            <h3 class="text-h5 mt-6 mb-2">Projeto Full-Stack em Equipe</h3>
+            <v-list>
+              <v-list-item>
+                <v-btn
+                  href="https://github.com/marcos-grocha/contact-book"
+                  target="_blank"
+                  rel="noopener"
+                  color="primary"
+                  variant="text"
+                >
+                E-Ventos | Treina-Dev 13
+                </v-btn>
+                <br><br>
+                Associado(s) a Campus Code <br>
+                Projeto integrado composto por três aplicações desenvolvidas em Ruby on Rails: events-app, speakers-app e participants-app.
+                <br><br>
+                Nossa equipe de desenvolvedores ficou responsável pela aplicação events-app (E-Ventos) <br>
+                O events-app é um sistema completo de gerenciamento de eventos, permitindo que organizadores criem e gerenciem eventos de forma eficiente. O aplicativo suporta a criação de contas de organizadores, cadastro de eventos, configuração de ingressos e agendas, além da verificação de usuários. Administradores têm acesso a ferramentas para moderar usuários, gerenciar categorias de eventos e configurar palavras-chave para facilitar buscas. O sistema também controla limites de participantes e lotes de ingressos, com diferentes opções e preços.
+                <br><br>
+                jan de 2025 - fev de 2025
+                <br><br>
+              </v-list-item>
+            </v-list>
+
           </v-card-text>
         </v-card>
       </v-col>
